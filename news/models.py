60741969f77ffc7304d86ace3fda6f9f00df39c0
SHIVAM YADAV <demo@gmail.com>
2023-07-30 13:09:37 +0530

    heh

--- news/models.py
+++ news/models.py
@@ -4,4 +4,9 @@ from tinymce.models import HTMLField
 # Create your models here.
 class newsModels(models.Model):
     news_title=models.CharField(max_length=200)
+<<<<<<< HEAD
     news_desc=HTMLField()
+=======
+    news_desc=HTMLField()
+    news_slug=AutoSlugField(populate_from="news_title",null=False,blank=False)
+>>>>>>> parent of 07c6ba5 (slug-problme solved)
